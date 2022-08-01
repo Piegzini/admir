086c20a85943ea61bfbc9f8c1f4b1f91b3207cbf
Konrad Piegza <piegzini.k@gmail.com>
2022-08-01 14:19:22 +0200

    Start to styling header
    Trying to start compose and styling header component with logo and navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import styles from '../../styles/navigation.module.sass'
+import logo from '../../assets/logo.png'
+import Image from "next/image";
+const linksTitles = ["GALERIA", "OFERTA", "O NAS", "KONTAKT"]
 const Navigation = () => {
     return (
         <header className={styles.header}>
-
+            <figure className={styles.logo}>
+              <Image src={logo} alt="Logo firmy Admir" />
+            </figure>
+            <nav>
+                <ul className={styles.navigation_elements}>
+                    {linksTitles.map( title => <li key={title} className={styles.navigation_element}>{title}</li>)}
+                </ul>
+            </nav>
         </header>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Navigation from "../components/Navigation/Navigation";
 
 export default function Home() {
   return (
@@ -9,10 +10,12 @@ export default function Home() {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta name="author" content="Konrad Piegza" />
             <meta name="keywords" content="ogrodzenia, panele, bramy, automaty, montaż" />
-            <meta name="color-scheme" content="light dark" />
+            <meta name="color-scheme" content="light" />
             <meta name="robots" content="index, follow /" />
             <meta name="language" content="Polish" />
         </Head>
+
+        <Navigation />
     </div>
   )
 }
