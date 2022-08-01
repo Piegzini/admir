9c9931337602fbc8603bca88f69ffacf98f5142b
Konrad Piegza <piegzini.k@gmail.com>
2022-08-01 13:48:51 +0200

    Add navigation comopnent
    I created navigation component in order to start coding and styling the site.

---
+++ components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from '../../styles/navigation.module.sass'
+const Navigation = () => {
+    return (
+        <header className={styles.header}>
+
+        </header>
+    );
+};
+
+export default Navigation;
