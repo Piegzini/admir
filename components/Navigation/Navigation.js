9218a6c9c47531641cd373bf0254c4765fcef4a7
Konrad Piegza <piegzini.k@gmail.com>
2022-08-02 15:29:21 +0200

    Add styles for active link in navigation

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -2,8 +2,12 @@ import React from 'react';
 import styles from '../../styles/navigation.module.sass'
 import logo from '../../assets/logo.png'
 import Image from "next/image";
-const linksTitles = ["GALERIA", "OFERTA", "O NAS", "KONTAKT"]
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+const linksTitles = [{title: "O NAS", link:"/"}, {title: "OFERTA", link:"/oferta"}, {title: "GALERIA", link:"/galeria"},   {title: "KONTAKT", link:"/kontakt"}]
 const Navigation = () => {
+    const router = useRouter()
     return (
         <header className={styles.header}>
             <figure className={styles.logo}>
@@ -11,7 +15,11 @@ const Navigation = () => {
             </figure>
             <nav>
                 <ul className={styles.navigation_elements}>
-                    {linksTitles.map( title => <li key={title} className={styles.navigation_element}>{title}</li>)}
+                    {linksTitles.map( ({title, link}) =>
+                        (<li key={title}
+                             className={[styles.navigation_element, router.pathname === link ? styles.active : ''].join(' ')}>
+                            <Link href={link}><a>{title}</a></Link>
+                        </li>))}
                 </ul>
             </nav>
         </header>
