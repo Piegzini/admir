eaa8b194cac7172fc9301f36d8b84c1b10992ea9
Konrad Piegza <piegzini.k@gmail.com>
2022-08-17 13:12:31 +0200

    Add main home page module

---
+++ components/Main/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './main.module.sass'
+import image from '../../assets/simon-maage-xw-vvvXq3rA-unsplash.jpg'
+const Main = props => {
+    return (
+        <section className={styles.banner} >
+            <div className={styles.content} >
+
+            </div>
+        </section>
+    );
+};
+
+Main.propTypes = {
+
+};
+
+export default Main;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from "next/head";
 import Navigation from "../components/Header/Header";
+import Main from "../components/Main/Main";
+
 
 export default function Home() {
   return (
     <div>
-
+      <Main />
 
     </div>
   )
