a5fe77deed5ece0a635a0c6dcdbdf2948efe2ac7
Konrad Piegza <konrad.piegza@ringieraxelspringer.pl>
2022-10-19 20:04:50 +0200

    Adding useWindowSize hook

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,28 +1,21 @@
-import React, {useEffect, useState} from 'react';
-import styles from '../../styles/navigation.module.sass'
-import logo from '../../assets/logo.png'
+import React from 'react';
+import styles from '../../styles/navigation.module.sass';
+import logo from '../../assets/logo.png';
 import Image from "next/image";
 import Navigation from "./Navigation /Navigation";
 import MobileNav from "./Mobilenav/MobileNav";
+import useWindowSize from "../../helpers/useWindowSize";
 
 const Header = () => {
-    const [windowWidth, setWindowWidth] = useState(0)
-
-    useEffect(() => {
-        setWindowWidth(window.innerWidth)
-        const handleResize = () => setWindowWidth(window.innerWidth)
-        window.addEventListener("resize", handleResize)
-        return () => window.removeEventListener("resize", handleResize)
-    }, []);
-
+    const [width] = useWindowSize();
 
     return (
         <header className={styles.header}>
             <figure className={styles.logo}>
-              <Image src={logo}  alt="Admir company logo" />
+                <Image src={logo} alt="Admir company logo"/>
             </figure>
 
-            { windowWidth < 768 ? <MobileNav /> : < Navigation /> }
+            {width < 768 ? <MobileNav/> : < Navigation/>}
 
 
         </header>
